50b22eb72c549aaa0fd2d2cb65f0465996d14beb
Jaikuon <84585927+jaikuun@users.noreply.github.com>
2024-05-17 17:01:20 -0400

    basic map

--- main.py
+++ main.py
@@ -5,8 +5,25 @@
 
 from urllib.request import urlopen
 import json
+import pandas as pd
+import csv
+import plotly.express as px
+
+
+result = pd.read_csv("election.csv")
+
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
 
-counties["features"][0]
-print(counties)
+df = pd.read_csv("election.csv", dtype={"county_fips": str})
+
+
+
+fig = px.choropleth(df, geojson=counties, locations='county_fips', color='per_point_diff',
+                           color_continuous_scale="bluered",
+                           range_color=(-1, 1),
+                           scope="usa",
+                           labels={'per_point_diff':'Per Point Difference'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
